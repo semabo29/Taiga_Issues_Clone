43fd4891f7a3fe6299712a701e7c996011b15dfb
VictorSalinasMontanuy <victor.salinas.montanuy@estudiantat.upc.edu>
2026-04-16 21:43:50 +0000

    feature(watchers): add another user as a watcher

--- app/controllers/watchings_controller.rb
+++ app/controllers/watchings_controller.rb
@@ -3,13 +3,28 @@ class WatchingsController < ApplicationController
   before_action :set_issue
 
   def create
-    @issue.watchers << current_user unless @issue.watchers.include?(current_user)
-    redirect_to @issue, notice: "Watching issue."
+    if params.key?(:user_id)
+      # Si le dio al "+" pero no seleccionó a nadie
+      if params[:user_id].blank?
+        redirect_to @issue # Redirigimos sin hacer nada
+        return
+      else
+        user_to_add = User.find(params[:user_id])
+      end
+    else
+      # Si viene del botón directo de "WATCH", usamos al usuario actual
+      user_to_add = current_user
+    end
+    
+    @issue.watchers << user_to_add unless @issue.watchers.include?(user_to_add)
+    redirect_to @issue, notice: "Watcher added."
   end
 
   def destroy
-    @issue.watchers.delete(current_user)
-    redirect_to @issue, notice: "Stopped watching issue."
+    user_to_remove = params[:user_id].present? ? User.find(params[:user_id]) : current_user
+    
+    @issue.watchers.delete(user_to_remove)
+    redirect_to @issue, notice: "Watcher removed."
   end
 
   private
