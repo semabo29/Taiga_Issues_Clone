48b1c36361601006bbe23b49804aec59482372b0
VictorSalinasMontanuy <victor.salinas.montanuy@estudiantat.upc.edu>
2026-05-04 20:55:16 +0000

    GET endpoint for issue details

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,32 +1,44 @@
 module Api
   module V1
     class IssuesController < Api::BaseController
+      before_action :set_issue, only: %i[show]
+
       # GET /api/v1/issues
       def index
-        #  Columes
+        # ordenar
         sortable_columns = ["issue_type_id", "severity_id", "priority_id", "id", "deadline", "status_id", "assigned_to_id", "user_id"]
         
-        # Ordre i direccio
         sort_column = sortable_columns.include?(params[:sort]) ? params[:sort] : "id"
         sort_direction = %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
         
-        # Aplicar el orden base
+        # aplicar ordre
         @issues = Issue.all.order("#{sort_column} #{sort_direction}")
 
-        # Cerca per text
+        # cerca per text
         if params[:query].present?
           search_term = "%#{params[:query]}%"
           @issues = @issues.where("subject LIKE ? OR description LIKE ?", search_term, search_term)
         end
 
-        # Altres filtres
+        # altres filtres
         @issues = @issues.where(issue_type_id: params[:issue_type_id]) if params[:issue_type_id].present?
         @issues = @issues.where(severity_id: params[:severity_id]) if params[:severity_id].present?
         @issues = @issues.where(priority_id: params[:priority_id]) if params[:priority_id].present?
         @issues = @issues.where(status_id: params[:status_id]) if params[:status_id].present?
 
-        # JSON
         render json: @issues
+      end
+
+      # GET /api/v1/issues/:id
+      def show
+        render json: @issue
+      end
+
+      private
+
+      def set_issue
+        # Si l'id no existeix retorna 404 pel base controller
+        @issue = Issue.find(params[:id])
       end
     end
   end
