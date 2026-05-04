039e99692f57a752d93d3fd94190dfd210700c26
VictorSalinasMontanuy <victor.salinas.montanuy@estudiantat.upc.edu>
2026-05-04 14:32:54 +0000

    install and configure rack-cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
-    # Aplicamos esta regla a todas las rutas (puedes restringirlo a '/api/*' si prefieres)
     resource '/api/*',
-      headers: :any,
-      
-      # Nivell 2
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    headers: :any,
+    # Nivell 2
+    methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
