dad18fe1d84180fb5428bcb29f76ffe0a5fd2bdc
VictorSalinasMontanuy <victor.salinas.montanuy@estudiantat.upc.edu>
2026-05-04 14:57:47 +0000

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,24 @@
 Rails.application.routes.draw do
-  # El núcleo de la aplicación
+  # === Rutes web ===
+  
   resources :issues do
     resource :watching, only: [:create, :destroy] 
-
     resources :comments, only: [:create, :edit, :update, :destroy]
 
-    # Definim rutes a nivell de col·lecció (no requereixen un ID d'issue previ)
     collection do
       get 'bulk_new'
       post 'bulk_create'
-
     end
     
-    # Definim rutes a nivell de membre (per a una issue específica)
     member do
       delete :purge_attachment
     end
   end
   
-  resources :users # Más adelante gestionaremos el perfil aquí
+  resources :users
 
-  # Apartado de Settings (Agrupado para el Lliurament)
-  # Esto hará que las URLs sean /settings/statuses, /settings/priorities...
   scope :settings do
-    get "/" => "settings#index", as: :settings # Ruta base de settings
+    get "/" => "settings#index", as: :settings
     resources :statuses
     resources :priorities
     resources :severities
@@ -32,12 +27,28 @@ Rails.application.routes.draw do
     resources :deadline_shortcuts 
   end
 
-  # Rutas de login del usuario
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
   delete '/logout', to: 'sessions#destroy', as: :logout
 
-  # Health check y Root
   get "up" => "rails/health#show", as: :rails_health_check
   root "issues#index"
+
+
+  # === API REST ===
+  # Namespace que agrupa las rutas bajo /api y espera les respostes en JSON
+  namespace :api, defaults: { format: :json } do
+    # v1
+    namespace :v1 do
+      resources :issues do
+        resources :comments, only: [:create, :update, :destroy]
+      end
+      resources :users, only: [:index, :show] 
+      resources :statuses
+      resources :priorities
+      resources :severities
+      resources :issue_types
+      resources :tags
+    end
+  end
 end
