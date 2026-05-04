f18d4b900b453d91bacd74622e20d8299f1cc1b1
VictorSalinasMontanuy <victor.salinas.montanuy@estudiantat.upc.edu>
2026-05-04 14:38:57 +0000

    modify controller

--- app/controllers/api/base.controller.rb
+++ app/controllers/api/base.controller.rb
@@ -7,6 +7,13 @@ module Api
     def authenticate_user_by_api_key
       # Busquem la clau a la capçalera 'X-Api-Key' o per paràmetre ?api_key=...
       api_key = request.headers['X-Api-Key'] || params[:api_key]
+
+      # MEJORA: Si no envían token en absoluto, abortamos antes de hacer la consulta SQL
+      if api_key.blank?
+        render json: { error: "No s'ha proporcionat cap API Key" }, status: :unauthorized
+        return
+      end
+
       @current_user = User.find_by(api_key: api_key)
 
       unless @current_user
