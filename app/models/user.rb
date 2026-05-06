96e8b08c5cab457e209b8ffd8fb0eb4014edf566
Claudiagalan3 <claudia.galan@estudiantat.upc.edu>
2026-05-06 11:22:47 +0000

    feat: fix api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,25 @@
 class User < ApplicationRecord
-  #No tocamos el id, Rails lo gestiona solo.
-  #Usamos el email para identificar al usuario de Google.
-  has_many :issues, dependent: :destroy #issues assignadas al usuario
+  # Relacions
+  has_many :issues, dependent: :destroy # Issues creades per l'usuari
   has_many :comments, dependent: :destroy
   has_many :assigned_issues, class_name: "Issue", foreign_key: "assigned_to_id"
-  has_many :issues, dependent: :destroy
+  
   has_many :watchings, dependent: :destroy
   has_many :watched_issues, through: :watchings, source: :issue
+  
   has_one_attached :avatar
+
+
   before_save :ensure_api_key
 
   def self.from_omniauth(auth)
-    #Buscamos por la columna email de la tabla
-    where(email: auth.info.email).first_or_create do |user|
-      #Si no existe, creamos el usuario con los datos de Google
-      user.username = auth.info.name || auth.info.email.split('@').first
+    user = where(email: auth.info.email).first_or_initialize do |u|
+      u.username = auth.info.name || auth.info.email.split('@').first
     end
+
+    user.ensure_api_key 
+    user.save
+    user
   end
   
   def ensure_api_key
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_17_081334) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_06_111216) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -137,6 +137,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_17_081334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.string "api_key"
   end
 
   create_table "watchings", force: :cascade do |t|
