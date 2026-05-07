16fc53c49542e4a27b3bff26c1b1f42ddd0d7c96
adriaaguilar25 <adria.aguilar@estudiantat.upc.edu>
2026-05-07 10:52:44 +0000

    arreglar problema

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
         resources :comments, only: [:index, :create]
         resources :attachments, only: [:index, :create]
         resource :deadline, only: [:show, :create, :destroy]
+        resources :activities, only: [:index]
       end
 
       resources :comments, only: [:update, :destroy]
